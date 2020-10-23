fa2de7f409dfae207cc3a813ffe6a049a9a95aef
Rebecca Kern <rkern@andrew.cmu.edu>
2020-10-23 15:15:53 -0400

    completed chapter 2

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,7 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -31,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener{ view: View ->
@@ -41,8 +44,21 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
         nextButton.setOnClickListener{
-            currentIndex = (currentIndex +1) %questionBank.size
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            if (currentIndex < 0) {
+                currentIndex = currentIndex + questionBank.size
+            }
             updateQuestion()
         }
 
